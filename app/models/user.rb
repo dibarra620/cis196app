8a531eb694dcfec7e1e61e03cfc3940e72b459b7
dibarra <dibarra620@gmail.com>
2013-11-11 16:56:03 -0500

    update bootstraps

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,19 @@ class User < ActiveRecord::Base
   validates :password, length: {within: 6..12 }
   emailFormatValidation = /[a-z.-]+@[a-z \d -.]+[.][a-z]+/i
   validates :email, format: {with: emailFormatValidation}
-  validates :email, uniqueness: true
+  #validates :email, uniqueness: true
   validates :password, confirmation: true
 
   def self.create_with_omniauth(auth)
   	create! do |user|
-  		user.provider = auth["provider"]
-  		user.uid = auth["uid"]
-  		user.name = auth["info"]["name"]
-  		user.email = "#{auth["info"]["nickname"]}@twitter.com"
-  		user.password = "noPassReq'd"
+  		    user.provider = auth["provider"]
+  		    user.uid = auth["uid"]
+  		    user.name = auth["info"]["name"]
+  		    user.email = "#{auth["info"]["nickname"]}@twitter.com"
+  		    user.password = "noPassReq'd"
+
 
   	end
   end
+
 end
